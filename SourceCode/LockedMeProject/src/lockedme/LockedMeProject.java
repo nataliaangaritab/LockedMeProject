4a54e86ea80eb94c7842c25be52a419a0df59ed5
Natalia <nataliaangaritab@gmail.com>
2021-08-16 13:43:56 -0500

    Update app

--- SourceCode/LockedMeProject/src/lockedme/LockedMeProject.java
+++ SourceCode/LockedMeProject/src/lockedme/LockedMeProject.java
@@ -2,6 +2,8 @@ package lockedme;
 
 public class LockedMeProject {
 
+	//Path
+	static final String folderpath="C:\\Users\\natal\\Desktop\\Simplilearn\\Phase1-LockedMeProject\\Files";
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
