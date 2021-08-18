12c771b62d1b31c3dd7103bbf0438cddd1a6b720
Natalia <nataliaangaritab@gmail.com>
2021-08-17 22:00:47 -0500

    Option to search a user specified file from the application

--- SourceCode/LockedMeProject/src/lockedme/LockedMeProject.java
+++ SourceCode/LockedMeProject/src/lockedme/LockedMeProject.java
@@ -1,26 +1,17 @@
 package lockedme;
 
-import java.util.Scanner;
+import java.util.List;
 
 public class LockedMeProject
 {
-	//Create path variable
+
+	//Path
 	static final String folderpath="C:\\Users\\natal\\Desktop\\Simplilearn\\Phase1-LockedMeProject\\Files";
 	public static void main(String[] args)
-	{
-		//Delete a file
-		String fileName;
-		Scanner obj = new Scanner(System.in);
-		System.out.println("Enter file name to be deleted: ");
-		fileName = obj.nextLine();
-		
-		boolean isDeleted = FileManager.deleteFile(folderpath, fileName);
-		if(isDeleted)
-			System.out.println("File deleted successfully!!");
-		else
-			System.out.println("File not in folder or some access issue");
-		
-		//Close Scanner object
-		obj.close();
+{
+		List<String> fileNames = FileManager.getFiles(folderpath);
+		for(String f:fileNames)
+			System.out.println(f);
 	}
+
 }
